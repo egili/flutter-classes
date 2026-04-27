3b49acd2b097e0259d8d536839e2a25a66baffad
Pietra Façanha Bortolato <pietra.bortolato@gmail.com>
2026-04-27 20:19:14 -0300

    Configuração inicial do Firebase e Firestore

--- pokedex_app/lib/main.dart
+++ pokedex_app/lib/main.dart
@@ -2,9 +2,21 @@
 // Pietra Façanha Bortolato - 25002436
 
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart'; // Importação do Firebase
+import 'firebase_options.dart'; // Importação das suas credenciais
 import 'home_screen.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  // Garante que os plugins do Flutter estejam prontos antes de iniciar o Firebase
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Inicializa o Firebase com as configurações do seu projeto
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
